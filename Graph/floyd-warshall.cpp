930978c68b6e0bff6e7cd3c8c514f0a9822686b3
Pradeep Suthar <49487927+sutharp777@users.noreply.github.com>
2020-05-11 21:30:55 +0530

    Update floyd-warshall.cpp

--- Graph/floyd-warshall.cpp
+++ Graph/floyd-warshall.cpp
@@ -4,24 +4,28 @@
 #define inf 0xfffffff
 using namespace std;
 
-void floyd_warshall(int G[][400], int &n){
-    for(int k=0; k<n; k++)  {
-       for(int i=0; i<n; i++)  {
-          for(int j=0; j<n; j++)  {
+void floyd_warshall(int G[][400], int &n)
+{
+    for(int k=0; k<n; k++)  
+    {
+       for(int i=0; i<n; i++)  
+       {
+          for(int j=0; j<n; j++)  
+          {
              G[i][j] = min(G[i][j], G[i][k] + G[k][j]);
           }
         }
     }
-
 }
 
-int main(){
+int main()
+{
     int n,w,x,y,e;
     // cout<<"input: ";
     cin>>n>>e;
 
     int G[n][400];
-    for(int i=0; i<n; i++){
+    for (int i=0; i<n; i++){
         fill_n(G[i],n,inf);
         G[i][i] = 0;
     }
