c414939c161bf517d5e85f7f32de3c628c8b6f8b
Pradeep Suthar <49487927+sutharp777@users.noreply.github.com>
2020-05-17 23:17:24 +0530

    Update CRT.cpp

--- Number Theory/CRT.cpp
+++ Number Theory/CRT.cpp
@@ -1,58 +1,10 @@
 #include <bits/stdc++.h>
 #include <algorithm>
 
-struct Graph{
-    int v;
-    std::set<int> *edj_list;
-    Graph(int n){
-        int v = n+1;
-        edj_list = new std::set<int>[n+1];
-
-    }
-    void add_edj(int v, int u){
-        edj_list[v].insert(u);
-        edj_list[u].insert(v);
-    }
-
-    void remove_edj(int u, int v){
-        edj_list[v].erase(u);
-        edj_list[u].erase(v);
-    }
-
-    void eular_path(int u){
-        if(edj_list[u].empty()){
-            std::cout<<u<<" :";
-            return;
-        }
-        int v = *edj_list[u].begin();
-        std::cout << u <<" -> ";
-        remove_edj(v,u);
-        eular_path(v);
-    }
-
-    void eular_path2(int u){
-        std::stack<int> s;
-        s.push(u);
-
-        while(!s.empty()) {
-            u = s.top();
-            s.pop();
-            if (edj_list[u].empty()){
-                std::cout << u << " :\n";
-                break;
-            }
-            int v = *edj_list[u].begin();
-            s.push(v);
-            std::cout << u <<" -> ";
-            remove_edj(v,u);
-        }
-    }
-
-
-};
-
-long long gcd(long long a, long long b, long long &x, long long &y){
-   if(a==0){
+long long gcd(long long a, long long b, long long &x, long long &y)
+{
+   if(a==0)
+   {
        x = 0;
        y = 1;
        return b;
@@ -64,25 +16,14 @@ long long gcd(long long a, long long b, long long &x, long long &y){
    return d;
 }
 
-void func(){
-    Graph graph(4);
-    graph.add_edj(1,2);
-    graph.add_edj(1,4);
-    graph.add_edj(2,4);
-    graph.add_edj(2,3);
-    graph.add_edj(3,4);
-    graph.eular_path2(2);
-    long long x,y;
-    std::cout << gcd(77,31, x, y)<<"\n";
-    std::cout<<(x+31)%31<<"\n";
-
-}
-
-class CRT{
+class CRT
+{
     long long pro;
     long long ans;
 public:
-    CRT(std::vector<long long> a, std::vector<long long> m){
+    
+    CRT(std::vector<long long> a, std::vector<long long> m)
+    {
         int n = a.size();
         long long x;
         std::vector<long long> M(n);
@@ -91,11 +32,13 @@ public:
         pro = 1ll;
         ans = 0ll;
 
-        for(int i = 0 ; i < n; i++){
+        for(int i = 0 ; i < n; i++)
+        {
             pro *= m[i];
         }
 
-        for(int i = 0 ; i < n; i++){
+        for(int i = 0 ; i < n; i++)
+        {
             M[i] = pro/m[i];
             y[i] = pow(M[i], m[i]-2, m[i]);
             ans = ans%pro +  (M[i]*y[i]*a[i]) % pro;
@@ -104,10 +47,13 @@ public:
 
     }
 
-    long long pow(long long a, long long b, long long mod){
+    long long pow(long long a, long long b, long long mod)
+    {
         long long ans = 1ll;
-        while(b){
-            if(b&1){
+        while(b)
+        {
+            if(b&1)
+            {
                 ans *= a;
                 ans %= mod;
             }
@@ -118,19 +64,22 @@ public:
         return ans;
     }
 
-    long long getAns(){
+    long long getAns()
+    {
         return ans;
     }
 };
 
-int run(int x){
+int run(int x)
+{
     std::queue<int> que;
     que.push(3);
     que.push(7);
 
 
     int a, count = 0;
-    while(que.front() <= x){
+    while(que.front() <= x)
+    {
         count++;
         a = que.front();
         que.pop();
@@ -140,11 +89,12 @@ int run(int x){
     return count;
 }
 
-int main() {
+int main()
+{
 
-//    CRT crt({2, 3, 2},{3, 5, 7});
+    CRT crt({2, 3, 2},{3, 5, 7});
 
-//    std::cout<< crt.getAns()<<"\n";
-    std::cout<< run(773);
+    std::cout<< crt.getAns()<<"\n";
+//    std::cout<< run(773);
     return 0;
 }
