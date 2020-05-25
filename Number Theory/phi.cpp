7563da1c848a276cca28268dfe0c9f8fc74039e9
Pradeep Suthar <49487927+sutharp777@users.noreply.github.com>
2020-05-26 01:20:03 +0530

    Update phi.cpp

--- Number Theory/phi.cpp
+++ Number Theory/phi.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
-
+#include<iostream>
+#define MAX 1000102
+unsigned long long phi[MAX + 7];
 	//Complexity : Sqrt(N)
 int phi(int n) {
 	int result = n;
@@ -16,39 +17,33 @@ int phi(int n) {
 
 	return result;
 }
+	//Complexity : O(N*loglongN)
 
-void computeTotient(int n) 
-{ 
-	long long phi[n+1]; 
-	for (int i=1; i<=n; i++) 
-		phi[i] = i;
+/**
+ * It took 0.902 secs to generate up to 1e7.
+**/
+void get(){
+    phi[1] = 1;
+	for (int i = 2; i <= MAX; i++) {
+        if(!phi[i]) {
+            phi[i] = i-1;
+            for (int j = 2*i; j <= MAX; j += i) {
+                if (!phi[j])
+			phi[j] = j;
+				
+                phi[j] = (phi[j] * (i-1)) / i;
+            }
+        }
+    }
 
-	for (int p=2; p<=n; p++) 
-	{ 
-		// If phi[p] is not computed already, 
-		// then number p is prime 
-		if (phi[p] == p) 
-		{
-			// Phi of a prime number p is 
-			// always equal to p-1. 
-			phi[p] = p-1; 
-			// Update phi values of all 
-			// multiples of p 
-			for (int i = 2*p; i<=n; i += p) 
-			{ 
-			// Add contribution of p to its 
-			// multiple i by multiplying with 
-			// (1 - 1/p) 
-			phi[i] = (phi[i]/p) * (p-1); 
-			} 
-		} 
-	} 
-
-	for (int i=1; i<=n; i++) 
-		std::cout << "Totient of " << i << " is " << phi[i] << '\n'; 
-} 
-int main(){
-	//std::cout<<phi(18);
-	computeTotient(18);
+}
+int main() {
+	int q, n;
+	scanf("%d",&q);
+	get();
+	while(q--){
+		scanf("%d",&n);
+		printf("%llu\n",phi[n]);
+	}
 	return 0;
 }
